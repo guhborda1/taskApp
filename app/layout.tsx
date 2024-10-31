1f954c36bc0df52e74c97216e2153d32717db321
guhborda1 <guhborda@gmail.com>
2024-10-30 21:33:31 -0300

    fix on files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "./_components/app-sidebar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
