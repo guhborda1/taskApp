bfdca74c8c7dc310b40aed305290ce5bcc252bf6
guhborda1 <guhborda@gmail.com>
2024-11-01 12:00:44 -0300

    fix on stripe

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -15,7 +15,7 @@ export const {
         verifyRequest: '/auth/signin',
         newUser: '/dashboard'
     },
-    adapter: PrismaAdapter(db),
+    adapter: PrismaAdapter(db) as Adapter,
     // providers: [Google, EmailProvider()],
     secret: process.env.SECRET,
     ...authConfig,
