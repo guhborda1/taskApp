6fa725dacdf893b5028d5518c44473b9b776b0ae
guhborda1 <guhborda@gmail.com>
2024-11-01 11:43:39 -0300

    fix

--- services/stripe/index.ts
+++ services/stripe/index.ts
@@ -17,7 +17,7 @@ type Plans = {
   [key: string]: Plan
 }
 export const stripe = new Stripe(config.stripe.secretKey || '', {
-  apiVersion: '2024-06-20',
+  apiVersion: '2024-09-30.acacia',
   httpClient: Stripe.createFetchHttpClient(),
 })
 
