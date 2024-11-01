e56b55ad2728f9dbd82e1bfa86de9c2a0ee258d6
guhborda1 <guhborda@gmail.com>
2024-11-01 11:16:45 -0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
 export { auth as middleware } from "@/services/auth"
+import { NextRequest, NextResponse } from 'next/server'
+import { getUrl } from './lib/getUrl'
+export default function middleware(req: NextRequest) {
+    const token = req.cookies.get('authjs.session-token')
+    const pathname = req.nextUrl.pathname
+    console.log({
+        token: token?.value,
+        pathname,
+    })
+    if (pathname === '/auth/signin' && token) {
+        return NextResponse.redirect(new URL(getUrl('/')))
+    }
+}
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
